b518e2b082eed5182e544be7de1c8100af34286e
AleksasLT <alex.yanishauskas@gmail.com>
2021-10-13 14:56:25 +0200

    added new files

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 $(document).ready(function(){
   /*maskedinput*/
-  jQuery(function($){
+  /* jQuery(function($){
     $.mask.definitions["h"] = "[А-Яа-я]";
+    //$.mask.definitions['g'] = "[A-Fa-f0-9А-Яа-я]";
     $(".phone").mask("+7 (999) 999-99-99", {autoclear: false});
     $(".name").mask("hhh?hhhhhh", {placeholder: ""});
-});
+    $(".email").mask("***?@****?.**?", {placeholder: ""});
+}); */
 
 /*initialisation slider*/
 $('.popup-btn').on('click', function (e) {
   e.preventDefault();
-  $('.popup').show('slow');
+  $('.popup').toggleClass('open');
 });
 $('.popup-close').on('click', function (e) {
  e.preventDefault();
- $('.popup').hide();
+ $('.popup').toggleClass('open');
 });
+$('.popup-ok_close').on('click', function (e) {
+  e.preventDefault();
+  $('.popup-ok').toggleClass('open');
+ });
 /*Feedback slider*/
 $('.feedback-slider').slick({
   prevArrow: '<button type="button" class="feedback-slider-btn feedback-prev-btn"><img src="./img/feedback/prevArrow.png"/></button>',
@@ -102,4 +108,152 @@ $('.certificate-slider').slick({
 // instead of a settings object
 ]
 });
+/*Modal form*/
+$('.modal_form').validate({
+    errorClass: "input-error",
+    rules: {
+      // simple rule, converted to {required:true}
+      modal_username: "required",
+      modal_userphone: "required"
+        },
+    messages: {
+         modal_username: "*Напишите ваше имя",
+         modal_userphone: "*В этом поле укажите ваш телефон в формате: +7(XXX)XXX-XX-XX",
+         modal_usertext: "*В этом поле напишите ваше сообщение"
+        },
+      submitHandler: function(form) {
+        var formData = form;
+        var formData = new FormData(formData);
+        $.ajax({
+        url: "./php/modal.php",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (msg) {
+                  $(form)[0].reset(),
+                  console.log(msg);
+                  $('.popup').toggleClass('open');
+                  $('.popup-ok').toggleClass('open');
+        } 
+      });
+      return false;
+    }
+  });
+$('.value_form').validate({
+    errorClass: "input-error",
+    rules: {
+      // simple rule, converted to {required:true}
+      value_username: "required",
+      value_userphone: "required"
+        },
+        messages: {
+          value_username:"*Напишите ваше имя",
+          value_userphone:"*В этом поле укажите ваш телефон в формате: +7(XXX)XXX-XX-XX"
+        },
+      submitHandler: function(form) {
+        var formData = form;
+        var formData = new FormData(formData);
+        $.ajax({
+        url: "./php/value.php",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (msg) {
+                  $(form)[0].reset(),
+                  console.log(msg);
+                  $('.popup-ok').toggleClass('open');
+        } 
+      });
+      return false;
+    }
+  });
+$('.feedback_form').validate({
+    errorClass: "input-error",
+    rules: {
+      // simple rule, converted to {required:true}
+      feedback_username: "required",
+      feedback_userphone: "required"
+        },
+    messages: {
+      feedback_username: "*Напишите ваше имя",
+      feedback_userphone: "*В этом поле укажите ваш телефон в формате: +7(XXX)XXX-XX-XX"
+        },
+      submitHandler: function(form) {
+        var formData = form;
+        var formData = new FormData(formData);
+        $.ajax({
+        url: "./php/feedback.php",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (msg) {
+                  $(form)[0].reset(),
+                  console.log(msg);
+                  $('.popup-ok').toggleClass('open');
+        } 
+      });
+      return false;
+    }
+  });
+$('.tour_form').validate({
+    errorClass: "input-error",
+    rules: {
+      // simple rule, converted to {required:true}
+      tour_username: "required",
+      tour_userphone: "required"
+        },
+    messages: {
+      tour_username: "*Напишите ваше имя",
+      tour_userphone: "*В этом поле укажите ваш телефон в формате: +7(XXX)XXX-XX-XX"
+        },
+      submitHandler: function(form) {
+        var formData = form;
+        var formData = new FormData(formData);
+        $.ajax({
+        url: "./php/tour.php",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (msg) {
+                  $(form)[0].reset(),
+                  console.log(msg);
+                  $('.popup-ok').toggleClass('open');
+        } 
+      });
+      return false;
+    }
+  });
+$('.contacts_form').validate({
+    errorClass: "input-error",
+    rules: {
+      // simple rule, converted to {required:true}
+      contacts_username: "required",
+      contacts_userphone: "required"
+        },
+    messages: {
+      contacts_username: "*Напишите ваше имя",
+      contacts_userphone: "*В этом поле укажите ваш телефон в формате: +7(XXX)XXX-XX-XX"
+        },
+      submitHandler: function(form) {
+        var formData = form;
+        var formData = new FormData(formData);
+        $.ajax({
+        url: "./php/contacts.php",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (msg) {
+                  $(form)[0].reset(),
+                  console.log(msg);
+                  $('.popup-ok').toggleClass('open');
+        } 
+      });
+      return false;
+    }
+  });
 });
